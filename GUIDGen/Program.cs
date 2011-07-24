da150c728d80529083d827743ceb2a2a3de9e57c
David Guinnip <dguinnip@askpri.org>
2011-07-24 17:23:41 -0400

    First commit

---
+++ GUIDGen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GUIDGen
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            System.Windows.Forms.Clipboard.SetText(Guid.NewGuid().ToString());
+        }
+    }
+}
